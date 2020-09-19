290c01f66d54e96c38cddb3c5e7502a3eb03426d
MustafaGamal000 <mustafaa.gamal000@gmil.com>
2020-09-19 15:39:19 +0200

    Add Slider

--- app/src/main/java/com/example/bloodfinder/LoginPage.java
+++ app/src/main/java/com/example/bloodfinder/LoginPage.java
@@ -2,13 +2,29 @@ package com.example.bloodfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginPage extends AppCompatActivity {
-
+private TextView intoRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intoRegister=findViewById(R.id.into_register_txt);
+        intoRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRegisterPage();
+            }
+        });
+    }
+
+    private void openRegisterPage() {
+        Intent intent =new Intent(this, RegisterPage.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/bloodfinder/RegisterPage.java
+++ app/src/main/java/com/example/bloodfinder/RegisterPage.java
@@ -2,13 +2,36 @@ package com.example.bloodfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.r0adkll.slidr.Slidr;
+import com.r0adkll.slidr.model.SlidrInterface;
 
 public class RegisterPage extends AppCompatActivity {
-
+    private SlidrInterface slidrInterface;
+private TextView intoLoginPage_tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        slidrInterface =Slidr.attach(this);
+
+        intoLoginPage_tv=findViewById(R.id.into_login_tv);
+        intoLoginPage_tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLoginPage();
+            }
+        });
+    }
+
+    private void openLoginPage() {
+        Intent intent =new Intent(this, LoginPage.class);
+        startActivity(intent);
+        finish();
     }
 }
