7ec269bc257f86b00a2fe53ea4fc562e349d8d5d
MustafaGamal000 <mustafaa.gamal000@gmil.com>
2020-09-20 00:27:53 +0200

    commit

--- app/src/main/java/com/example/bloodfinder/RegisterPage.java
+++ app/src/main/java/com/example/bloodfinder/RegisterPage.java
@@ -5,22 +5,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.r0adkll.slidr.Slidr;
 import com.r0adkll.slidr.model.SlidrInterface;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class RegisterPage extends AppCompatActivity {
     private SlidrInterface slidrInterface;
-private TextView intoLoginPage_tv;
+    private TextView intoLoginPage_tv;
+    private EditText username_et, email_et, phone_et, password_et,rePassword;
+    private Spinner bloodGroup;
+    private Button register_btn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        slidrInterface =Slidr.attach(this);
+        slidrInterface = Slidr.attach(this);
 
-        intoLoginPage_tv=findViewById(R.id.into_login_tv);
+        username_et=findViewById(R.id.username_et);
+        email_et=findViewById(R.id.email_et);
+        phone_et=findViewById(R.id.mobile_et);
+        password_et=findViewById(R.id.password_et);
+        rePassword=findViewById(R.id.re_password_et);
+        bloodGroup=findViewById(R.id.blood_group_spinner);
+        register_btn=findViewById(R.id.register_btn);
+
+
+        register_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        intoLoginPage_tv = findViewById(R.id.into_login_tv);
         intoLoginPage_tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,8 +56,11 @@ private TextView intoLoginPage_tv;
     }
 
     private void openLoginPage() {
-        Intent intent =new Intent(this, LoginPage.class);
+        Intent intent = new Intent(this, LoginPage.class);
         startActivity(intent);
         finish();
+    }
+    private void registerNewAccount(String username, String email, String phoneNumber, String bloodGroup, String password){
+
     }
 }
